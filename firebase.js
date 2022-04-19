2394a905f8edd80b37afec984ccb1ef3434cca99
serj321 <serj1232012@hotmail.com>
2022-04-19 17:09:44 -0400

    moved api keys to .env file
    more secure code

--- firebase.js
+++ firebase.js
@@ -1,8 +1,10 @@
 import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth"
 import { initializeApp } from "firebase/app";
 
+
+console.log(process.env.FIREBASE_API_KEY)
 const firebaseConfig = {
-  apiKey: "AIzaSyDcZva4xZxgr6hLIItE9btTJU6ew-0s8Rw",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "nextproject-249dd.firebaseapp.com",
   databaseURL: "https://nextproject-249dd-default-rtdb.firebaseio.com",
   projectId: "nextproject-249dd",
--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    MONGO_URI: "mongodb+srv://serjSililian:6I3glAACZ7srUq4X@nextAppCluster.upwau.mongodb.net/todosNext?retryWrites=true&w=majority"
-  }
 }
 
 module.exports = nextConfig
